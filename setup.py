87bc089bad295b0777df3b66a7b2d46abaff6917
Ghanshyam <gopalagr85@gmail.com>
2024-10-03 18:58:53 +0530

    test

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name='my_pip_package',
+    name='custom_pip_package',
     version='1.0.0',
 
-    url='https://github.com/gopalagr85/pip_package/custom_pip_package',
+    url='https://github.com/gopalagr85/pip_package',
     author='Ghanshyam',
     author_email='gopalagr85@gmail.com',
 
-    py_modules=['my_pip_package'],
+    packages=find_packages(),
 )
