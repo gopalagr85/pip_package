8e645a8e170a83dd6a27bb8b06451624bd7a0bc1
Ghanshyam <gopalagr85@gmail.com>
2024-10-03 18:53:44 +0530

    test41

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='my_pip_package',
     version='1.0.0',
 
-    url='https://github.com/gopalagr85/pip_package',
+    url='https://github.com/gopalagr85/pip_package/custom_pip_package',
     author='Ghanshyam',
     author_email='gopalagr85@gmail.com',
 
