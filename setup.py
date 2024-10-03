c9b0c1e3f004332617cd7feec1d5ae0286909c66
gopalagr85 <gopalagr85@gmail.com>
2024-10-03 18:08:23 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pip_package',
+    version='1.0.0',
+
+    url='https://github.com/gopalagr85/pip_package',
+    author='Ghanshyam',
+    author_email='gopalagr85@gmail.com',
+
+    py_modules=['pip_package'],
+)
